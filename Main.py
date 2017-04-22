20af829977a2ce77bf9e6a6784c436aa20eddebc
Mvrcu <Marcusg56@icloud.com>
2017-04-22 00:59:33 -0500

    Sprite collision

--- Main.py
+++ Main.py
@@ -4,24 +4,23 @@
 # Main.py
 
 import os
-import random
 import sys
+import random
 
 import pygame as pg
 
 
-CAPTION = "Catch the money with the arrow keys"
+CAPTION = "Move me with the Arrow Keys"
 SCREEN_SIZE = (800, 550)
 
 TRANSPARENT = (0, 0, 0, 0)
 
-score = 0
-money_array = []
 # This global constant serves as a very useful convenience for me.
 DIRECT_DICT = {pg.K_LEFT  : (-1, 0),
                pg.K_RIGHT : ( 1, 0),
                pg.K_UP    : ( 0,-1),
                pg.K_DOWN  : ( 0, 1)}
+
 
 def load_images():
     """Loads all images in 'img' folder for use on call."""
@@ -40,23 +39,46 @@ def load_images():
             'bonus_card': load_image('bonus_card.png'),
             'saw': load_image('saw.png')}
 
-class Player(object):
-    """
-    This class will represent our user controlled character.
-    """
-    SIZE = (100, 100)
 
-    def __init__(self, pos, speed):
+class Money(pg.sprite.Sprite):
+    def __init__(self, screen_rect, *groups):
         """
         The pos argument is a tuple for the center of the player (x,y);
         speed is given in pixels/frame.
         """
-        self.rect = pg.Rect((0,0), Player.SIZE)
-        self.rect.center = pos
-        self.speed = speed
-        self.image = load_images()
+        super(Money, self).__init__(*groups)
+        self.image = IMAGES["money"]
+        self.rect = self.image.get_rect()
+        self.reset(screen_rect)
 
-    def update(self, keys, screen_rect):
+    def reset(self, screen_rect):
+        self.speed = random.randrange(1, 5)
+        self.rect.y = random.randrange(-300, -self.rect.h)
+        self.rect.x = random.randrange(0, screen_rect.w - self.rect.w)
+
+    def update(self, screen_rect, *args):
+        self.rect.y += self.speed
+        if self.rect.top > screen_rect.h:
+            self.reset(screen_rect)
+
+
+class Player(pg.sprite.Sprite):
+    """
+    This class will represent our user controlled character.
+    """
+
+    def __init__(self, pos, speed, *groups):
+        """
+        The pos argument is a tuple for the center of the player (x,y);
+        speed is given in pixels/frame.
+        """
+        super(Player, self).__init__(*groups)
+        self.image = IMAGES["wallet"]
+        self.rect = self.image.get_rect(center=pos)
+        self.speed = speed
+        self.score = 0
+
+    def update(self, screen_rect, keys):
         """
         Updates our player appropriately every frame.
         """
@@ -64,13 +86,7 @@ class Player(object):
             if keys[key]:
                 self.rect.x += DIRECT_DICT[key][0]*self.speed
                 self.rect.y += DIRECT_DICT[key][1]*self.speed
-        self.rect.clamp_ip(screen_rect) # Keep player on screen.
-
-    def draw(self, surface):
-        """
-        Blit image to the target surface.
-        """
-        surface.blit(self.image['wallet'], self.rect)
+        self.rect.clamp_ip(screen_rect)
 
 
 class Application(object):
@@ -88,7 +104,11 @@ class Application(object):
         self.fps = 60
         self.done = False
         self.keys = pg.key.get_pressed()
-        self.player = Player(self.screen_rect.center, 5)
+        self.allsprites = pg.sprite.Group()
+        self.object_sprites = pg.sprite.Group()
+        self.player = Player(self.screen_rect.center, 5, self.allsprites)
+        for _ in range(15):
+            Money(self.screen_rect, self.allsprites, self.object_sprites)
 
     def event_loop(self):
         """
@@ -101,17 +121,16 @@ class Application(object):
                 self.done = True
             elif event.type in (pg.KEYUP, pg.KEYDOWN):
                 self.keys = pg.key.get_pressed()
-
+        hit_list = pg.sprite.spritecollide(self.player, self.allsprites, True)
+        for i in hit_list:
+            self.player.score +=1
+            print(self.player.score)
     def render(self):
         """
         Perform all necessary drawing and update the screen.
         """
         self.screen.fill(pg.Color("white"))
-        # Select the font to use, size, bold, italics
-        font = pg.font.SysFont('Calibri', 25, True, False)
-        text = font.render("Score: "+ str(score), True, (0, 0, 0))
-        self.screen.blit(text, [5, 5])
-        self.player.draw(self.screen)
+        self.allsprites.draw(self.screen)
         pg.display.update()
 
     def main_loop(self):
@@ -120,7 +139,7 @@ class Application(object):
         """
         while not self.done:
             self.event_loop()
-            self.player.update(self.keys, self.screen_rect)
+            self.allsprites.update(self.screen_rect, self.keys)
             self.render()
             self.clock.tick(self.fps)
 
@@ -128,10 +147,12 @@ def main():
     """
     Prepare our environment, create a display, and start the program.
     """
+    global IMAGES
     os.environ['SDL_VIDEO_CENTERED'] = '1'
     pg.init()
     pg.display.set_caption(CAPTION)
     pg.display.set_mode(SCREEN_SIZE, pg.NOFRAME)
+    IMAGES = load_images()
     Application().main_loop()
     pg.quit()
     sys.exit()
